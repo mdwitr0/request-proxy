a5f0f3a977118bc2ae272bdc0a77259048788a04
Evgenii Fedoryachenko <109113769+mdwitr0@users.noreply.github.com>
2022-11-20 01:11:19 +0000

    add "get" cases

--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/go-resty/resty/v2"
-	"net/http"
 )
 
 type ProxyQuery struct {
@@ -20,23 +21,38 @@ func proxyHandle(c *gin.Context) {
 	err := c.ShouldBindJSON(&body)
 	if err != nil {
 		fmt.Println(err)
-		c.JSON(http.StatusOK, "")
+		c.JSON(http.StatusInternalServerError, "")
 	}
 
-	client := resty.New()
+	client := resty.New().EnableTrace()
+
+	if body.Headers != nil {
+		client.SetHeaders(body.Headers)
+	}
+
+	if body.Params != nil {
+		client.SetQueryParams(body.Params)
+	}
 
 	var result interface{}
-	_, err = client.R().
-		SetBody(body.Data).
-		SetHeaders(body.Headers).
-		SetQueryParams(body.Params).
-		SetResult(&result).
-		EnableTrace().
-		Post(body.Url)
+
+	switch body.Method {
+	case "POST":
+		_, err = client.R().
+			SetBody(body.Data).
+			SetResult(&result).
+			Post(body.Url)
+		break
+	case "GET":
+		_, err = client.R().
+			SetResult(&result).
+			Get(body.Url)
+		break
+	}
 
 	if err != nil {
 		fmt.Println(err)
-		c.JSON(http.StatusOK, "")
+		c.JSON(http.StatusInternalServerError, "")
 	}
 
 	c.JSON(http.StatusOK, result)
@@ -45,7 +61,7 @@ func proxyHandle(c *gin.Context) {
 func main() {
 	r := gin.Default()
 	r.POST("", proxyHandle)
-	err := r.Run()
+	err := r.Run(":8001")
 	if err != nil {
 		return
 	}
